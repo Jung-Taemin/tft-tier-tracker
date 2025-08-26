22dff8c38b27fcf21275d0e6810674d5b6a9319e
taemin <jungtaemin1214@naver.com>
2025-08-26 15:44:35 +0900

    changes

---
+++ src/main/java/me/jungtaemin/tfttiertracker/TftTierTrackerApplication.java
@@ -0,0 +1,13 @@
+package me.jungtaemin.tfttiertracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TftTierTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TftTierTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/jungtaemin/tfttiertracker/TftTierTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jungtaemin.tfttiertracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TftTierTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
